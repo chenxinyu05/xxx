c3805c6887ee7d2feb71fe07bd2a6c06d2f78692
chenxinyu05 <chenxinyu05@chenxinyu05s-MacBook-Pro.local>
2026-06-23 21:22:07 +0800

    1

--- main.cpp
+++ main.cpp
@@ -10,10 +10,10 @@
 #include "xxx/xxx_i32stack.h"
 
 int main() {
-    xxx_string_t s;
-    xxx_string_init(&s);
-    xxx_string_assign(&s, "abcde", 5);
-    xxx_string_append(&s, xxx_string_cstr(&s), xxx_string_length(&s));
-    printf("%zu %s\n", xxx_string_length(&s), xxx_string_cstr(&s));
+    // xxx_string_t s;
+    // xxx_string_init(&s);
+    // xxx_string_assign(&s, "abcde", 5);
+    // xxx_string_append(&s, xxx_string_cstr(&s), xxx_string_length(&s));
+    // printf("%zu %s\n", xxx_string_length(&s), xxx_string_cstr(&s));
 }
 
